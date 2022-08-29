a4a466f3d5a98b28fdbee8eca2011231cdf73868
vivek <ikumarvivek786@gmail.com>
2022-08-29 20:22:10 +0530

    Class

--- Fellowship/Program.cs
+++ Fellowship/Program.cs
@@ -11,6 +11,19 @@ namespace Fellowship
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Fellowship");
+         
+            //maruti object
+            Car maruti = new Car();
+            maruti.color = "red";
+            maruti.price = "70000";
+            maruti.model = "baleno";
+            maruti.display();
+            //bmw objext
+            bmw.color = "white";
+            bmw.price = "90000"
+            bmw.model = "x5";
+            bmw.display();
             Console.ReadLine();
-;        }    }
+                }
+    }
 }
