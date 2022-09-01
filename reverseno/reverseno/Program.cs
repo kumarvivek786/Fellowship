a0e56613e47b8bf88011242ad747b29062e33c47
vivek <ikumarvivek786@gmail.com>
2022-09-01 19:52:35 +0530

    reverseno

--- reverseno/reverseno/Program.cs
+++ reverseno/reverseno/Program.cs
@@ -10,6 +10,21 @@ namespace reverseno
     {
         static void Main(string[] args)
         {
+
+            int num = 1234;
+            int result = 0;
+            int reminder;
+
+            while (num != 0)
+            {
+                reminder = num % 10;
+
+                result = result*10 + reminder;
+
+                num = num / 10;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
