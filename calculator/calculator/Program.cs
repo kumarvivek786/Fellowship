1bb3ba572db6b6ff3bedf5af36fb4c21f90a60a1
vivek <ikumarvivek786@gmail.com>
2022-09-08 18:58:43 +0530

    calculator

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int res;
+            Console.WriteLine("enter no. 1 :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter no. 2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter sumbol(+,-,/,*):");
+            string symbol = Console.ReadLine();
+
+            switch(symbol)
+            {
+                case "+":
+                    res = num1 + num2;
+                    Console.WriteLine("Addition :" + res);
+                    break;
+
+                case "-":
+                    res = num1 - num2;
+                    Console.WriteLine("subtraction :" + res);
+                    break;
+
+                case "*":
+                    res = num1 * num2;
+                    Console.WriteLine("multiplication :" + res);
+                    break;
+
+                case "/":
+                    res = num1 / num2;
+                    Console.WriteLine("Division :" +res);
+                    break;
+
+                default:
+                    Console.WriteLine("wrong input");
+                    break;
+            }
+                    Console.ReadLine();
+
+
+
+        }
+    }
+}
