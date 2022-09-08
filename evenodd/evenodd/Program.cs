872413ef1b20a5321c6efc63fabb56f07bd548c3
vivek <ikumarvivek786@gmail.com>
2022-09-08 19:37:59 +0530

    evenodd no

---
+++ evenodd/evenodd/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenodd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            
+            Console.WriteLine("enter no. :");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num%2==0)
+            {
+                Console.WriteLine("Prime Number");
+            }
+            
+            else
+            {
+                Console.WriteLine("Odd Number");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
