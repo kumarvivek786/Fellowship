634f92e741fc61f67bd1cc3503ab52ba387f3bbd
vivek <ikumarvivek786@gmail.com>
2022-09-14 19:03:02 +0530

    uc4

--- Empwageuc1/Program.cs
+++ Empwageuc1/Program.cs
@@ -10,38 +10,32 @@ namespace Empwageuc1
     {
         static void Main(string[] args)
         {
-            //UC-3
-            int FULL_TIME = 1;
-            int PART_TIME = 2;
-            int EMP_RATE_PER_HR = 20;
+            //UC-4 
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
+            const int EMP_RATE_PER_HR = 20;
             int empHrs = 0,empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             
             Console.WriteLine("Random value:" + empCheck);
 
-            if(FULL_TIME == empCheck)
+            switch(empCheck)
             {
-                empHrs = 8;
-
-                Console.WriteLine("FullTIme Employee is Present");
+                case FULL_TIME: 
+                    empHrs = 8;
+                    Console.WriteLine("FullTime Employee is Present");
+                    break;
+                case PART_TIME:
+                    empHrs = 4;
+                    Console.WriteLine("PartTime Employee is Present");
+                    break;                
+                default:
+                    break;
 
             }
 
-            else if (PART_TIME == empCheck)
-            {
-                empHrs = 4;
-
-                Console.WriteLine("PartTime Employee is Present");
-
-            }
-            else
-            { 
-
-                empHrs = 0;
-                Console.WriteLine("Employee is Absent");
-            }
-                empWage = empHrs * EMP_RATE_PER_HR; ;
+              empWage = empHrs * EMP_RATE_PER_HR; ;
             Console.WriteLine("Employee Wage Per Day:" + empWage);
 
                 Console.ReadLine();
