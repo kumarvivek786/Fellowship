69ae37ce635137b9445c2bede7c32313ba6d2bb6
vivek <ikumarvivek786@gmail.com>
2022-09-15 20:04:19 +0530

    uc6

--- Empwageuc1/Program.cs
+++ Empwageuc1/Program.cs
@@ -10,30 +10,31 @@ namespace Empwageuc1
     {
         static void Main(string[] args)
         {
-            //UC-5 
+            //UC-6 
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int MAX_WORKING_DAYS = 20;
-            int empHrs = 0,empWage = 0,totalEmpwage= 0;
+            const int MAX_WORKING_HRS = 100;
+            int empHrs = 0, empWage = 0, totalEmpwage = 0, day = 1,totalHrs=0; 
             Random random = new Random();
 
-            for (int day=1;day<=MAX_WORKING_DAYS;day++)
+            while(day<MAX_WORKING_DAYS && totalHrs<=MAX_WORKING_HRS)
             {
 
                 int empCheck = random.Next(0, 3);
 
-                Console.WriteLine("Random value:" + empCheck);
+                //Console.WriteLine("Random value:" + empCheck);
 
                 switch (empCheck)
                 {
                     case FULL_TIME:
                         empHrs = 8;
-                        Console.WriteLine("FullTime Employee is Present");
+                       // Console.WriteLine("FullTime Employee is Present");
                         break;
                     case PART_TIME:
                         empHrs = 4;
-                        Console.WriteLine("PartTime Employee is Present");
+                        //Console.WriteLine("PartTime Employee is Present");
                         break;
                     default:
                         break;
@@ -41,10 +42,13 @@ namespace Empwageuc1
                 }
 
                 empWage = empHrs * EMP_RATE_PER_HR; ;
-                Console.WriteLine("Employee Wage on Day{0} is {1}",day,empWage);
+                totalHrs+=empHrs;
+                Console.WriteLine("Employee Wage on Day{0} and Hrs{1} is {2}",day,empHrs,empWage);
                 //totalEmpwage = totalEmpwage + empWage;
                 totalEmpwage+=empWage;
-
+                day++;
+                totalHrs+= empHrs;
+                 
             }
             Console.WriteLine("\nTotal Empwage is;" + totalEmpwage);
                 Console.ReadLine();
