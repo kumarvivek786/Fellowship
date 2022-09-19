e59ed009c3adb727fa434dbd62e81394eb5d322e
vivek <ikumarvivek786@gmail.com>
2022-09-19 14:02:32 +0530

    UC-7 compute empwage using class method

--- Empwageuc1/Program.cs
+++ Empwageuc1/Program.cs
@@ -10,35 +10,53 @@ namespace Empwageuc1
     {
         static void Main(string[] args)
         {
-            //UC-4 
+            CalculateEmpWage();
+            Console.ReadLine();
+        }
+        
+        public static void CalculateEmpWage()
+        {
+            //UC-7 
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
-            int empHrs = 0,empWage = 0;
+            const int MAX_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 100;
+            int empHrs = 0, empWage = 0, totalEmpwage = 0, day = 1, totalHrs = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            
-            Console.WriteLine("Random value:" + empCheck);
 
-            switch(empCheck)
+            while (day<= MAX_WORKING_DAYS && totalHrs<= MAX_WORKING_HRS)
             {
-                case FULL_TIME: 
-                    empHrs = 8;
-                    Console.WriteLine("FullTime Employee is Present");
-                    break;
-                case PART_TIME:
-                    empHrs = 4;
-                    Console.WriteLine("PartTime Employee is Present");
-                    break;                
-                default:
-                    break;
+
+                int empCheck = random.Next(0, 3);
+
+                //Console.WriteLine("Random value:" + empCheck);
+
+                switch (empCheck)
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        // Console.WriteLine("FullTime Employee is Present");
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        //Console.WriteLine("PartTime Employee is Present");
+                        break;
+                    default:
+                        break;
+
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HR; ;
+                totalHrs += empHrs;
+                Console.WriteLine("Employee Wage on Day{0} and Hrs {1} is {2}", day, totalHrs, empWage);
+                //totalEmpwage = totalEmpwage + empWage;
+                totalEmpwage += empWage;
+                day++;
+                
 
             }
-
-              empWage = empHrs * EMP_RATE_PER_HR; ;
-            Console.WriteLine("Employee Wage Per Day:" + empWage);
-
-                Console.ReadLine();
+            Console.WriteLine("\nTotal Empwage is;" + totalEmpwage);
         }
     }
 }
